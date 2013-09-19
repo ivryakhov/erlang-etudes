ed9258d5749d0672605f6f7a1c1e4496c4397ed9
ivryakhl <ivryakhov@gmail.com>
2013-09-19 06:15:22 +0000

    Étude 3-1: Pattern Matching, Étude 3-2: Guards

--- geom.erl
+++ geom.erl
@@ -2,20 +2,26 @@
 %%% @author Ryakhov Ivan <ivryakhov@gmail.com>
 %%% @copyright 2013
 %%% @doc 'Etudes for Erlang' exercise. Simple module and function.
-%%%      http://chimera.labs.oreilly.com/books/1234000000726/ch02.html
+%%%       [http://chimera.labs.oreilly.com/books/1234000000726]
 %%% @end
 %%%-------------------------------------------------------------------
 -module(geom).
--export([area/2]).
+-export([area/3]).
 -author('ivryakhov').
--revision('Revision: 1.0').
+-revision('Revision: 1.1').
 -created('Date: 2013/09/13').
+-modified('Date: 2013/09/19').
 -created_by('ivryakhov').
 
 %%-------------------------------------------------------------------
-%% @doc Returns the area of rectangle with Length and Width values.
-%% @spec area(Length::integer(), Width::integer()) -> integer()
+%% @doc Returns the area of the shape declared in a first parameter
+%%      using dimentions from two other parameters.
+%% @spec area(Shape::atom(), A::number(), B::number) -> number
 %% @end
 %%-------------------------------------------------------------------
-area(Length, Width) ->
-    Length * Width.
+area(Shape, A, B) when A >= 0, B >= 0 ->
+    case Shape of
+        rectangle   -> A * B;
+        triangle    -> A * B / 2.0;
+        ellipse     -> A * B * math:pi()
+    end.
