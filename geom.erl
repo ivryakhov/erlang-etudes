3ef69cb6e35eacf3a0377797700e20210b22eee8
ivryakhl <ivryakhov@gmail.com>
2013-09-19 07:07:45 +0000

    Étude 3-4: Tuples as Parameters

--- geom.erl
+++ geom.erl
@@ -6,12 +6,21 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(geom).
--export([area/3]).
+-export([area/1]).
 -author('ivryakhov').
--revision('Revision: 1.1').
+-revision('Revision: 1.2').
 -created('Date: 2013/09/13').
 -modified('Date: 2013/09/19').
 -created_by('ivryakhov').
+
+%%-------------------------------------------------------------------
+%% @doc Transforms given tuple to three parameters and
+%%      calls the private function area/3
+%% @spec area({Shape::atom(), A::number(), B::number()}) -> number()
+%% @end
+%%-------------------------------------------------------------------
+area({Shape, A, B}) -> area(Shape, A, B).
+
 
 %%-------------------------------------------------------------------
 %% @doc Returns the area of the shape declared in a first parameter
