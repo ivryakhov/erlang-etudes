e6381da1faa92215e19eaa26a14e234e9d22c6f7
ivryakhl <ivryakhov@gmail.com>
2013-10-17 06:57:29 +0000

    Étude 6-3: Accumulating the Sum of a List

--- dates.erl
+++ dates.erl
@@ -6,11 +6,48 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(dates).
--export([date_parts/1]).
--revision('Revision: 0.1').
+-export([date_parts/1, julian/1]).
+-revision('Revision: 0.2').
 -created('Date: 2013/10/14').
--modified('Date: 2013/10/14').
+-modified('Date: 2013/10/16').
 -created_by('ivryakhov').
+
+%%-------------------------------------------------------------------
+%% @doc Takes a string in ISO date format ("yyyy-mm-dd") and returns 
+%%      the Julian date: the day of the year
+%% @spec julian(string()) -> integer()
+%% @end
+%%-------------------------------------------------------------------
+julian(Date) ->
+    [Year, Month, Day ] = date_parts(Date),
+    DaysPerMonth = [31, days_in_feb(Year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    julian(Month, Day, DaysPerMonth, 0).
+
+%%-------------------------------------------------------------------
+%% @doc Helper function for julian/1 that takes 4 arguments:
+%%      month, day, list of days per month and total days count.
+%%      Return the number of day in a year.
+%% @spec julian(integer(), integer, list(integer()), integer()) -> integer()
+%% @end
+%%-------------------------------------------------------------------
+julian(1, Day, _, Total) ->
+    Day + Total;
+julian(Month, Day, [H|T], Total) when Month < 13 ->
+    julian(Month - 1, Day, T, Total + H);
+julian(_, _, _, _) ->
+    io:format("Wrong input data~n").
+
+%%-------------------------------------------------------------------
+%% @doc Takes a year number and returns the number days in Febriary
+%% @spec days_in_feb(integer()) -> integer()
+%% @end
+%%-------------------------------------------------------------------
+days_in_feb(Year) ->
+    case (Year rem 4 == 0 andalso Year rem 100 /= 0)
+          orelse (Year rem 400 == 0) of
+        true -> 29;
+        false -> 28
+    end.
 
 %%-------------------------------------------------------------------
 %% @doc Takes a string in ISO date format ("yyyy-mm-dd") and returns 
