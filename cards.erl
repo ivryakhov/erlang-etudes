c7b3242b69048809a3dd86c9a907e87ab9d928c5
ivryakhl <ivryakhov@gmail.com>
2013-10-21 21:05:11 +0000

    Étude 7-6: Explaining an Algorithm

--- cards.erl
+++ cards.erl
@@ -1,13 +1,13 @@
 %%%-------------------------------------------------------------------
 %%% @author Ryakhov Ivan <ivryakhov@gmail.com>
 %%% @copyright 2013
-%%% @doc 'Etudes for Erlang' exercises. Chapter 6. Lists, 
-%%%         Etude 7-3: Using lists:foldl/3
+%%% @doc 'Etudes for Erlang' exercises. Etude 7-5: Multiple Generators 
+%%%         in List Comprehensions, Etude 7-6: Explaining an Algorithm
 %%%       [http://chimera.labs.oreilly.com/books/1234000000726]
 %%% @end
 %%%-------------------------------------------------------------------
 -module(cards).
--export([make_deck/0]).
+-export([make_deck/0,shuffle/1]).
 -revision('Revision: 0.1').
 -created('Date: 2013/10/22').
 -modified('Date: 2013/10/22').
@@ -22,3 +22,24 @@ make_deck() ->
     Suits = ["Clubs", "Diamonds", "Hearts", "Spades"],
     Ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
     [{Rank, Suit} || Rank <- Ranks, Suit <- Suits].
+
+%%-------------------------------------------------------------------
+%% @doc Shuffles a list (deck of cards in this case)
+%% @spec shuffle(list()) -> list()
+%% @end
+%%-------------------------------------------------------------------
+shuffle(List) -> shuffle(List, []).
+
+%%-------------------------------------------------------------------
+%% @doc helper function for shuffle/1. Takes a random position in an
+%%      input list, splits list on two parts, gets the head element
+%%      from second part when and put it to accumalation list. Calls
+%%      themself after that with concatenation of first part and tail
+%%      from second part
+%% @spec shuffle(list(), list()) -> list()
+%% @end
+%%-------------------------------------------------------------------
+shuffle([], Acc) -> Acc;
+shuffle(List, Acc) ->
+    {Leading, [H | T]} = lists:split(random:uniform(length(List)) - 1, List),
+    shuffle(Leading ++ T, [H | Acc]).
