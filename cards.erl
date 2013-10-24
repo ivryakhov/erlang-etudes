ed1468410d40070127c4492a4ffb71b3547c9e52
ivryakhl <ivryakhov@gmail.com>
2013-10-24 07:36:17 +0000

    Starting Étude 8-1. Spawning dealer and player

--- cards.erl
+++ cards.erl
@@ -7,11 +7,41 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(cards).
--export([make_deck/0,shuffle/1]).
+-export([make_deck/0,shuffle/1, start_the_game/0, dealer/0, player/2]).
 -revision('Revision: 0.1').
 -created('Date: 2013/10/22').
 -modified('Date: 2013/10/22').
 -created_by('ivryakhov').
+
+start_the_game() ->
+    Dealer = spawn(cards, dealer, []),
+    Players = ["Andrea", "Bertram"],
+    Dealer ! {start_the_game, Players}.
+
+dealer() ->
+    receive
+        {start_the_game, Players} ->
+            io:format("Dealer: starting the game...~n"),
+            Deck = shuffle(make_deck()),
+            io:format("Dealer: the deck is: ~p~n", [Deck]),
+            NumCards = round(length(Deck) / length(Players)),
+            PlayersId = spawning_players(Players, Deck, NumCards, [])
+    end.
+
+player(Name, Cards) ->
+    receive
+        {start_the_game} ->
+            io:format("~s: I am ready for the game. My cards: ~p~n", [Name, Cards])
+    end.
+
+spawning_players([], _, _, PlayersId) ->
+    PlayersId;
+spawning_players([CurPlayer|Others], Deck, NumCards, PlayersId) ->
+    {PlayerCards,OtherDeck} = lists:split(NumCards, Deck),
+    CurPlayerId = spawn(cards, player, [CurPlayer, PlayerCards]),
+    CurPlayerId ! {start_the_game},
+    spawning_players(Others, OtherDeck, NumCards, PlayersId ++ [CurPlayerId]).
+
 
 %%-------------------------------------------------------------------
 %% @doc Generates a deck of cards as a list 52 tuples
@@ -20,7 +50,8 @@
 %%-------------------------------------------------------------------
 make_deck() ->
     Suits = ["Clubs", "Diamonds", "Hearts", "Spades"],
-    Ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+    %Ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"],
+    Ranks = ["A", "2", "3", "4"],
     [{Rank, Suit} || Rank <- Ranks, Suit <- Suits].
 
 %%-------------------------------------------------------------------
