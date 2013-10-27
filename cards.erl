e908757b96f7b4187a5fc1d0ff84690b662079a9
ivryakhl <ivryakhov@gmail.com>
2013-10-27 14:31:24 +0000

    Étude 8-1. Completed

--- cards.erl
+++ cards.erl
@@ -7,14 +7,14 @@
 %%% @end
 %%%-------------------------------------------------------------------
 -module(cards).
--export([make_deck/0,shuffle/1, start_the_game/0, dealer/3, player/2]).
--revision('Revision: 0.1').
+-export([start_the_game/0, dealer/3, player/2]).
+-revision('Revision: 0.3').
 -created('Date: 2013/10/22').
--modified('Date: 2013/10/22').
+-modified('Date: 2013/10/27').
 -created_by('ivryakhov').
 
 start_the_game() ->
-    PlayersNames = ["Andrea", "Bertram"],
+    PlayersNames = ["Andrea", "Bertram", "Bill", "Alice"],
     Dealer = spawn(cards, dealer, [[], [], []]),
     Dealer ! {start_the_game, PlayersNames}.
 
@@ -22,14 +22,16 @@ dealer(Pile, BottomCards, Players) ->
     Dealer = self(),
     receive
         {start_the_game, PlayersNames} ->
+            random:seed(now()),
             io:format("Dealer: starting the game...~n"),
             Deck = shuffle(make_deck()),
             io:format("Dealer: the deck is: ~p~n", [Deck]),
             NumCards = round(length(Deck) / length(PlayersNames)),
             NewPlayers = spawning_players(PlayersNames, Deck, NumCards, []),
-            Dealer ! {pre_battle, NewPlayers},
+            Dealer ! {pre_battle},
             dealer([], [], NewPlayers);
-        {pre_battle, Players} ->
+
+        {pre_battle} ->
             io:format("Dealer: pile is: ~p~n", [Pile]),
             io:format("Dealer: ~p give me your cards:~n", [Players]),
             case Pile of
@@ -39,26 +41,56 @@ dealer(Pile, BottomCards, Players) ->
             lists:map(fun({_Name, PlayerId}) -> PlayerId ! {give_me_cards, CardsNumber, Dealer} end,
                       Players),
             dealer(Pile, [], Players);
-        {awaiting_battle} ->
-            case length(BottomCards) < length(Players) of
+
+        {awaiting_battle, {Name, PlayerPid}, TopCards} ->
+            case TopCards of
+                [] ->
+                    io:format("Dealer: ~s don't have more cards and left the game.~n", [Name]),
+                    PlayerPid ! {left_the_game},
+                    NewPlayers = lists:delete({Name, PlayerPid}, Players),
+                    NewBottomCards = BottomCards,
+                    NewPile = Pile;
+                _ ->
+                    io:format("Dealer: ~s have sent ~p~n", [Name, TopCards]),
+                    NewPile = lists:append(Pile,TopCards),
+                    BottomCard = lists:last(TopCards),
+                    NewBottomCards = BottomCards ++ [{{Name, PlayerPid}, BottomCard}],
+                    NewPlayers = Players
+            end,
+            case length(NewBottomCards) < length(NewPlayers) of
                 true ->
-                    io:foprmat("Dealer: pile is: ~p~n", [Pile]),
-                    dealer(Pile, BottomCards, Players);
+                    dealer(NewPile, NewBottomCards, NewPlayers);
                 false ->
                     io:format("Dealer: all cards received.~nPile:~p~nBottomCards: ~p~n",
-                              [Pile, BottomCards]),
+                              [NewPile, NewBottomCards]),
                     Dealer ! {check_cards},
-                    dealer(Pile, BottomCards, Players)
+                    dealer(NewPile, NewBottomCards, NewPlayers)
             end;
+
         {check_cards} ->
-            Winners = find_winner(BottomCards, 0, []),
-            io:format("Dealer: battle winners: ~p~n", [Winners]);
-            
-        {here_are_cards, Name, TopCards} ->
-            io:format("Dealer: ~s have sent ~p~n", [Name, TopCards]),
-            BottomCard = lists:last(TopCards),
-            Dealer ! {awaiting_battle},
-            dealer(Pile ++ [TopCards], BottomCards ++ [{Name, BottomCard}], Players)
+            case length(Players) of
+                0 ->
+                    io:format("Dealer: there is no winners!~n");
+                1 ->
+                    {Name, _PlayerId} = hd(Players),
+                    io:format("Dealer: the winner is ~s. My congratulations!~n", [Name]);
+                _ ->
+                    Winners = find_winner(BottomCards, 0, []),
+                    io:format("Dealer: battle winners: ~p~n", [Winners]),
+                    case length(Winners) of
+                        1 ->
+                            {Name, PlayerPid} = hd(Winners),
+                            io:format("Dealer: the winner is ~s, Take your cards.~n", [Name]),
+                            PlayerPid ! {take_your_cards, Pile},
+                            Dealer ! {pre_battle},
+                            dealer([], [], Players);
+                        _ ->
+                            io:format("Dealer: It is a war. Give me 3 cards elese~n"),
+                            Dealer ! {pre_battle},
+                            dealer(Pile, [], Players)
+                    end
+            end
+
     end.
 
 player(Name, Cards) ->
@@ -67,9 +99,29 @@ player(Name, Cards) ->
             io:format("~s: I am ready for the game. My cards: ~p~n", [Name, Cards]),
             player(Name, Cards);
         {give_me_cards, CardsNumber, Dealer} ->
-            {TopCards, Rest} = lists:split(CardsNumber, Cards),
-            io:format("~s: Here is my card: ~p~n", [Name, TopCards]),
-            Dealer ! {here_are_cards, Name, TopCards}
+            case Cards of
+                [] ->
+                    TopCards = [],
+                    Rest = [],
+                    io:format("~s: I have no more cards.~n", [Name]);
+                _ ->
+                    case length(Cards) >= CardsNumber of
+                        true ->
+                            {TopCards, Rest} = lists:split(CardsNumber, Cards);
+                        false ->
+                            TopCards = Cards,
+                            Rest = []
+                    end,
+                    io:format("~s: Here is my card: ~p~n", [Name, TopCards])
+            end,
+            Dealer ! {awaiting_battle, {Name, self()}, TopCards},
+            player(Name, Rest);
+        {take_your_cards, NewCards} ->
+            UpdatedCards = lists:append([Cards, NewCards]),
+            io:format("~s: Here is my cards now: ~p~n", [Name, UpdatedCards]),
+            player(Name, UpdatedCards);
+        {left_the_game} ->
+            io:format("~s: Im leaving the game. Bye.~n", [Name])
     end.
 
 spawning_players([], _, _, PlayersId) ->
