98bdab5f7911a7b8986112247aaabfece69dc3dd
ivryakhl <ivryakhov@gmail.com>
2013-10-30 19:36:27 +0000

    Étude 10-1: Using ETS. Part Two

--- phone_ets.erl
+++ phone_ets.erl
@@ -5,10 +5,18 @@
 setup(FileName) ->
     case file:open(FileName, [read]) of
         {ok, InputFile} ->
-            TableId = ets:new(calls, [bag]),
-            process_file(InputFile, TableId);
+            TableId = ets:new(phone_calls, [bag]),
+            process_file(io:get_line(InputFile, ""), InputFile, TableId),
+            file:close(InputFile);
         {error, Why} ->
             {error, Why}
     end.
 
-process_file(InputFile, TableId) ->
+process_file(eof, _, _) ->
+    ok;
+process_file(Line, InputFile, TableId) ->
+    [PhoneNumber, StartingDate, StartingTime, EndDate, EndTime]
+         = re:split(Line, "[,\n]", [trim, {return, list}]),
+    ets:insert(TableId, #calls{phone_number = PhoneNumber, starting_date = StartingDate,
+            starting_time = StartingTime, end_date = EndDate, end_time = EndTime}), 
+    process_file(io:get_line(InputFile, ""), InputFile, TableId).
