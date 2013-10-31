d509e1d2c6b0ebf9bce0f373f0e7f4707c5b5e44
ivryakhl <ivryakhov@gmail.com>
2013-10-31 19:31:01 +0000

    Étude 10-1: Using ETS. Converting dates

--- phone_ets.erl
+++ phone_ets.erl
@@ -17,6 +17,12 @@ process_file(eof, _, _) ->
 process_file(Line, InputFile, TableId) ->
     [PhoneNumber, StartingDate, StartingTime, EndDate, EndTime]
          = re:split(Line, "[,\n]", [trim, {return, list}]),
-    ets:insert(TableId, #calls{phone_number = PhoneNumber, starting_date = StartingDate,
-            starting_time = StartingTime, end_date = EndDate, end_time = EndTime}), 
+    ets:insert(TableId, #calls{phone_number = PhoneNumber, starting_date = string_to_tuple(StartingDate),
+            starting_time = string_to_tuple(StartingTime),
+            end_date = string_to_tuple(EndDate), end_time = string_to_tuple(EndTime)}), 
     process_file(io:get_line(InputFile, ""), InputFile, TableId).
+
+string_to_tuple(String) ->
+    StrNum = re:split(String, "[-:]", [{return, list}]),
+    [Num1, Num2, Num3] = lists:map(fun(Str) -> {Num, _Rest} = string:to_integer(Str), Num end, StrNum),
+    {Num1, Num2, Num3}.
